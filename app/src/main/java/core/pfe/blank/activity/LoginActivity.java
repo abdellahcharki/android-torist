b1d38a079bb7f799bf93a6211680f8aa0eca11a1
Abdella charki <abdellah.charki@yahoo.com>
2021-04-30 10:12:01 +0000

    make log in  activty

---
+++ app/src/main/java/core/pfe/blank/activity/LoginActivity.java
@@ -0,0 +1,34 @@
+package core.pfe.blank.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import core.pfe.blank.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn ;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn=findViewById(R.id.login_btn);
+
+
+        intent=new Intent(this,MainActivity.class);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+    }
+}
